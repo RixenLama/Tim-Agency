e9c07e9cc7e7517ced6f3b5ad57b4f41c1f793e9
Rixen <Rixenlama@gmail.com>
2023-06-27 17:11:44 +0300

    Nav bar done

---
+++ src/Components/navBar.jsx
@@ -0,0 +1,33 @@
+
+const NavBar = () => {
+
+    return (
+        <div className="navbar">
+            <div className="nav-container">
+                <div>
+                    <img src="./TIM-logo.svg" alt="" />
+                </div>
+
+                <div >
+                    <ul className="horizontal-list nav-ul">
+                        <li className="WHITE ESTEBAN nav-item">
+                            Portfolio
+                        </li>
+                        <li className="WHITE ESTEBAN nav-item">
+                            Pricing
+                        </li>
+                        <li className="WHITE ESTEBAN nav-item">
+                            About us
+                        </li>
+                        <li className="WHITE ESTEBAN nav-item">
+                            Get a quote
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div> 
+    )
+
+}
+
+export default NavBar
