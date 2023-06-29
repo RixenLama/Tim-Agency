602ce49ff70f0d1b9abd241944f665192c0a7ae2
Rixen <Rixenlama@gmail.com>
2023-06-29 17:07:34 +0300

    Spline addition

---
+++ src/Pages/heroPage.jsx
@@ -0,0 +1,17 @@
+import Spline from "@splinetool/react-spline"
+import exp from "constants"
+
+const HeroPage = () => {
+    return(
+        <div>
+            <div>
+                <Spline scene="https://prod.spline.design/5WvAPcZsKG3IGDV0/scene.splinecode"/>
+            </div>
+            <div>
+            <button className="SMALLBUTTON ESTEBAN WHITE BUTTONSIZE">Submit</button>
+            </div>
+        </div>
+    )
+}
+
+export default HeroPage 
--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,10 +1,12 @@
 import NavBar from "../Components/navBar.jsx"
 import ContactForm from "../Components/contactForm.jsx"
+import HeroPage from "./heroPage.jsx"
 
 const HomePage = () => {
     return ( 
         <>
             <NavBar />
+            <HeroPage />
             <ContactForm />
         </>
     )
