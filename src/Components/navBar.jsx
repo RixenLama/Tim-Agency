5f7eed89b9125ff0ea9ec6647d6844f472588146
Rixen <Rixenlama@gmail.com>
2023-06-30 21:50:43 +0300

    responsive nav bad

--- src/Components/navBar.jsx
+++ src/Components/navBar.jsx
@@ -1,31 +1,69 @@
 
 const NavBar = () => {
 
+    const openNav = () => {
+        document.querySelector("#open-menu").style.display = "none"
+        document.getElementById("close-menu").style.display = "flex"
+        document.querySelector("#mobile-nav").style.display = "flex"
+    }
+
+    const closeNav = () =>{
+        document.querySelector("#open-menu").style.display = "flex"
+        document.getElementById("close-menu").style.display = "none"
+        document.querySelector("#mobile-nav").style.display = "none"
+    }
+
+
     return (
-        <div className="navbar">
-            <div className="nav-container">
-                <div>
-                    <img src="./TIM-logo.svg" alt="" />
+        <>
+            <div className="navbar">
+                <div className="nav-container">
+                    <div>
+                        <img src="./TIM-logo.svg" alt="" />
+                    </div>
+
+                    <div>
+                        <ul className="horizontal-list nav-ul">
+                            <li className="WHITE ESTEBAN nav-item">
+                                Portfolio
+                            </li>
+                            <li className="WHITE ESTEBAN nav-item">
+                                Pricing
+                            </li>
+                            <li className="WHITE ESTEBAN nav-item">
+                                About us
+                            </li>
+                            <li className="WHITE ESTEBAN nav-item">
+                                Get a quote
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="mobile-menu-container">
+                        <img id="open-menu" className="mobile-menu" src="./mobile-menu.svg" alt="" onClick={openNav} />
+                        <img id="close-menu" className="mobile-menu-exit" src="./mobile-menu-exit.svg" alt="" onClick={closeNav} />
+                    </div>
                 </div>
 
-                <div >
-                    <ul className="horizontal-list nav-ul">
-                        <li className="WHITE ESTEBAN nav-item">
-                            Portfolio
-                        </li>
-                        <li className="WHITE ESTEBAN nav-item">
-                            Pricing
-                        </li>
-                        <li className="WHITE ESTEBAN nav-item">
-                            About us
-                        </li>
-                        <li className="WHITE ESTEBAN nav-item">
-                            Get a quote
-                        </li>
-                    </ul>
+                <div id="mobile-nav" className="mobile-dropbar">
+                    <div>
+                        <ul className="mobile-nav-ul">
+                            <li className="WHITE ESTEBAN mobile-nav-li">
+                                Portfolio
+                            </li>
+                            <li className="WHITE ESTEBAN mobile-nav-li">
+                                Pricing
+                            </li>
+                            <li className="WHITE ESTEBAN mobile-nav-li">
+                                About us
+                            </li>
+                            <li className="WHITE ESTEBAN mobile-nav-li">
+                                Get a quote
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div> 
+        </>
     )
 
 }
