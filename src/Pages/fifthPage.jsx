751c397d6921d93e15d117cbf8c99a66126eed95
Rixen <Rixenlama@gmail.com>
2023-07-04 17:14:54 +0300

    fifth page, but bug

--- src/Pages/fifthPage.jsx
+++ src/Pages/fifthPage.jsx
@@ -6,21 +6,117 @@ const FifthPage = () =>{
                 <h1 className="WHITE MONTSERRAT TEXTCENTER fifth-page-title">Our Packages</h1>
                 <div className="fifth-page-card-container">
                     <div className="fifth-page-card-1">
-                        <h3>
-                            WHITEBOARD
-                        </h3>
+                        <div className="fifth-page-card-text-container">
+                            <h4 className="WHITE ESTEBAN">
+                                WHITEBOARD
+                            </h4>
+                            <h2 className="WHITE MONTSERRAT">
+                                €489
+                            </h2>
+                            <div className="fifth-page-card-stat-container">
+                                <span className="WHITE MAITREE">
+                                    High Converting Script
+                                </span>
+                                <span className="WHITE MAITREE">
+                                    Powerful Voice-Over 
+                                </span>
+                                <span className="WHITE MAITREE">
+                                    Engaging Animation
+                                </span>
+                                <span className="WHITE MAITREE">
+                                    Music & SFX
+                                </span>
+                                <span className="WHITE MAITREE">
+                                    2 X Revisions
+                                </span>
+                                <span className="WHITE MAITREE">
+                                    4+ Weeks Delivery
+                                </span>
+                            </div>
+                        </div>
+                        <div>
+                            <button className="WHITE ESTEBAN SMALLBUTTON">
+                                Purchase it
+                            </button>
+                        </div>
                     </div>
 
                     <div className="fifth-page-card-2">
-                        <h3>
-                            EXPLAINER
-                        </h3>
+                        <div className="fifth-page-card-text-container">
+                            <h4 className="WHITE ESTEBAN">
+                                EXPLAINER
+                            </h4>
+                            <h2 className="WHITE MONTSERRAT">
+                                €839
+                            </h2>
+                            <div className="fifth-page-card-stat-container">
+                                <span className="WHITE MAITREE">
+                                    High Converting Script
+                                </span>
+                                <span className="WHITE MAITREE">
+                                    Powerful Voice-Over 
+                                </span>
+                                <span className="WHITE MAITREE">
+                                    Engaging Animation
+                                </span>
+                                <span className="WHITE MAITREE">
+                                    Music & SFX
+                                </span>
+                                <span className="WHITE MAITREE">
+                                    2 X Revisions
+                                </span>
+                                <span className="WHITE MAITREE">
+                                    4+ Weeks Delivery
+                                </span>
+                            </div>
+                        </div>
+                        <div>
+                            <button className="WHITE ESTEBAN SMALLBUTTON">
+                                Purchase it
+                            </button>
+                        </div>
                     </div>
 
                     <div className="fifth-page-card-3">
-                        <h3>
-                            PREMIUM
-                        </h3>
+                        <div className="fifth-page-card-text-container">
+                            <h4 className="WHITE ESTEBAN">
+                                PREMIUM
+                            </h4>
+                            <h2 className="WHITE MONTSERRAT">
+                                €1399
+                            </h2>
+                            <div className="fifth-page-card-stat-container">
+                                <span className="WHITE MAITREE">
+                                    High Converting Script
+                                </span>
+                                <span className="WHITE MAITREE">
+                                    Powerful Voice-Over 
+                                </span>
+                                <span className="WHITE MAITREE">
+                                    Engaging Animation
+                                </span>
+                                <span className="WHITE MAITREE">
+                                    Music & SFX
+                                </span>
+                                <span className="WHITE MAITREE">
+                                    4 X Revisions
+                                </span>
+                                <span className="WHITE MAITREE">
+                                    8+ Weeks Delivery
+                                </span>
+                                <span className="WHITE MAITREE">
+                                    Custom Graphics 
+                                </span>
+                                <span className="WHITE MAITREE">
+                                    Premium Animation Team
+                                </span>
+                            </div>
+                        </div>
+                        <div>
+                            <button className="WHITE ESTEBAN SMALLBUTTON">
+                                Purchase it
+                            </button>
+                        </div>
                     </div>
 
                 </div>
