f5cd2b8bb0a54b7927d34549d4d83ca70585f722
Rixen <Rixenlama@gmail.com>
2023-08-04 14:01:42 +0300

    sflsdfks

--- src/Components/fifth-page-card-1.jsx
+++ src/Components/fifth-page-card-1.jsx
@@ -52,7 +52,7 @@ const CardOne = () =>{
 
 
     return(
-        <PayPalScriptProvider options={{ "client-id":"AVAe0f6St526NWzlYxOa3rPLw-YRcrikFp_2XCM1fWmjbxr7bV-m05huSrjM37I5VGQgy5--IkrGriP3" }}>
+        <PayPalScriptProvider options={{ "client-id": process.env.PAYPAL_CLIENT_ID}}>
             <div className="fifth-page-card-1">
                 <div className="fifth-page-card-text-container">
                             <h4 className="WHITE ESTEBAN">
